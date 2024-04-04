14e35a4724a4b1f0d559294cdc87054f5302f569
Rahul Chaudhary <rahulchaudhary301@gmail.com>
2024-04-04 17:42:21 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+	cors: {
+		//origin: "https://rahulscreenvideo.netlify.app",
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+
+
+io.on("connection", (socket) => {
+    console.log('User Join')
+
+
+
+
+
+    
+	socket.on("disconnect", () => {
+    console.log('User disconnect...') 
+	})
+
+
+})
+
+server.listen(5000, () => console.log("server is running on port 5000"))
